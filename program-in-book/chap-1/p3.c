b16bcc689468d159b6cefd46419b042bf2d5f681
Vikramaditya singh bhati <33588312+6-god@users.noreply.github.com>
2018-08-26 11:29:27 +0530

    1

--- program-in-book/chap-1/p3.c
+++ program-in-book/chap-1/p3.c
@@ -1,15 +1,16 @@
 /*
 https://github.com/vikram-bhati
 */
+
 #include <stdio.h>
 // print Fahrenheit Celsius table
 int main(int argc, char const *argv[]) {
-  int fahr,celsius;
+  float fahr,celsius;
   int lower=0,upper=300,step=20;
   fahr=lower;
   while (fahr <= upper) {
     celsius= 5 * (fahr-32) / 9 ;
-    printf("%d\t%d\n",fahr,celsius );
+    printf("%3.0f\t%6.1f\n",fahr,celsius );
     fahr=fahr+step;
   }
 
